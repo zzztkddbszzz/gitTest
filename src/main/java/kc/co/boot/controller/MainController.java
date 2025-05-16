f89f3752e55aea838d77dd0bdc3bd466c35f11ab
sang_yoon <dltkddbsfl@gmail.com>
2025-05-16 15:41:54 +0900

    특정 커밋으로 되돌리기..

--- src/main/java/kc/co/boot/controller/MainController.java
+++ src/main/java/kc/co/boot/controller/MainController.java
@@ -14,6 +14,7 @@ public class MainController {
 	public String main() {
 		log.info("hello lee sany yoon");
 		log.info("아무거나 추가");
+		log.info("브랜치 초기화 : 특정한 커밋의 상태로 완전히 되돌려 버리는 것");
 		return "{\"msg\":\"OK\"}";
 	}
 	
