faef6a568e94ea29188b815b6c8961b1beefdb72
sang_yoon <dltkddbsfl@gmail.com>
2025-05-16 15:59:18 +0900

    write 추가

--- src/main/java/kc/co/boot/controller/MainController.java
+++ src/main/java/kc/co/boot/controller/MainController.java
@@ -1,8 +1,11 @@
 package kc.co.boot.controller;
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,8 +23,13 @@ public class MainController {
 	
 	@GetMapping(value="/list")
 	public String list() {
-		
 		return "";
+	}
+	
+	// 신규개발
+	@PostMapping(value="/write")
+	public Map<String, Object>write(){
+		return null;
 	}
 	
 	
