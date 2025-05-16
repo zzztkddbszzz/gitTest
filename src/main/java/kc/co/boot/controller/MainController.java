f3b208eb71af3cfbd0f5e4375fe4bd2a99bff985
sang_yoon <dltkddbsfl@gmail.com>
2025-05-16 15:09:52 +0900

    로거 추가

--- src/main/java/kc/co/boot/controller/MainController.java
+++ src/main/java/kc/co/boot/controller/MainController.java
@@ -1,14 +1,19 @@
 package kc.co.boot.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
+	Logger log = LoggerFactory.getLogger(getClass());
+	
 	@GetMapping(value="/")
 	public String main() {
-		
+	
+		log.info("hello~");
 		return "{\"msg\":\"OK\"}";
 	}
 }
